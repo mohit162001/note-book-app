c00b701bb404c1a02733c372777f3310cef8e302
Mohit Devade <mohit.deavade123@gmail.com>
2024-02-20 15:10:16 +0530

    header added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from 'react';
+import './CSS/header.css'; 
+import { clearCredential } from '../helper';
+import { useNavigate } from 'react-router-dom';
 
 function Header() {
+  const navigate = useNavigate();
+  function handleLogout(){
+    clearCredential()
+    navigate('/login')
+
+  }
   return (
-    <header>
-        
+    <header className="header">
+      <h2 className="title">DASHBOARD</h2>
+      <button onClick={handleLogout} className="logout-button">Logout</button>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
