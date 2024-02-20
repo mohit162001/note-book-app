3dc564c867ae6fa5091f2a6b44214c36c4ed8a5c
Mohit Devade <mohit.deavade123@gmail.com>
2024-02-20 15:55:30 +0530

    active link highlighadded

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './CSS/sidebar.css';
+
 
 function SideBar() {
   return (
@@ -9,10 +10,10 @@ function SideBar() {
         <p className='sidebar-title'>Note Book</p>       
         <ul className="sidebar-ul">
           <li className="sidebar-list-item">
-            <Link className="add-note-btn" to="/note">New Note</Link>
+            <NavLink className={({ isActive }) => (isActive ? 'active' : 'inactive')} to="/note">New Note</NavLink>
           </li>
           <li className="sidebar-list-item">
-            <Link className="sidebar-link" to="/history">History</Link>
+            <NavLink className={({ isActive }) => (isActive ? 'active' : 'inactive')} to="/history">History</NavLink>
           </li>
         </ul>
       </nav>
