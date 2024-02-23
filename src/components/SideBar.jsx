c46d80351ff273ceab17ce5d5b23f628c80eb075
Mohit Devade <mohit.deavade123@gmail.com>
2024-02-23 16:39:56 +0530

    update button conditionaly added

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,20 +1,44 @@
-import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
-import './CSS/sidebar.css';
-
+import React, { useState } from "react";
+import { NavLink, useParams } from "react-router-dom";
+import "./CSS/sidebar.css";
 
 function SideBar() {
-  const [text,setText] = useState(true)
+  const { id } = useParams();
+  const [text, setText] = useState(true);
   return (
     <aside className="sidebar-container">
-      <nav className="sidebar">   
-        <p className='sidebar-title'>Note Book</p>       
+      <nav className="sidebar">
+        <p className="sidebar-title">Note Book</p>
         <ul className="sidebar-ul">
+          {id ? (
+            <li className="sidebar-list-item">
+              <NavLink
+                onClick={() => setText(true)}
+                className={({ isActive }) => (isActive ? "active" : "inactive")}
+                to={`${id}`}
+              >
+                Update Note
+              </NavLink>
+            </li>
+          ) : (
+            <li className="sidebar-list-item">
+              <NavLink
+                onClick={() => setText(true)}
+                className={({ isActive }) => (isActive ? "active" : "inactive")}
+                to="/"
+              >
+                {text ? "New Note" : "Add New Note"}
+              </NavLink>
+            </li>
+          )}
           <li className="sidebar-list-item">
-            <NavLink onClick={()=>setText(true)} className={({ isActive }) => (isActive ? 'active' : 'inactive')} to="/">{text ? 'New Note': "Add New Note"}</NavLink>
-          </li>
-          <li className="sidebar-list-item">
-            <NavLink onClick={()=>setText(false)} className={({ isActive }) => (isActive ? 'active' : 'inactive')} to="/history">History</NavLink>
+            <NavLink
+              onClick={() => setText(false)}
+              className={({ isActive }) => (isActive ? "active" : "inactive")}
+              to="/history"
+            >
+              History
+            </NavLink>
           </li>
         </ul>
       </nav>
