a56b4544b6b3c80d159a58da02e6263dc066abf4
Mohit Devade <mohit.deavade123@gmail.com>
2024-02-23 10:55:31 +0530

    updated login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,105 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
 import "./CSS/login.css";
 import { Link, useNavigate } from "react-router-dom";
 import { storeData } from "../helper";
 import { useMutation } from "@apollo/client";
 import { USER_LOGIN } from "../query/query";
-
+import { Alert, Snackbar } from "@mui/material";
 
 function Login() {
-
   const navigate = useNavigate();
-
-  const [mutationFun,{data,loading,error}] = useMutation(USER_LOGIN,{
-    onCompleted(data){
-      console.log("inside onComleted")
-      console.log(data)
-      storeData(data.login)
-      navigate("/")
+  const [mutationFun, { data, loading, error }] = useMutation(USER_LOGIN, {
+    onCompleted(data) {
+      console.log("inside onComleted");
+      console.log(data);
+      storeData(data.login);
+      navigate("/");
     },
-    onError(error){
-      window.alert("somethng wrong....!")
-      console.log(error)
-    }
-  })
+    onError(error) {
+      window.alert("somethng wrong....!");
+      console.log(error);
+    },
+  });
+
 
   async function handleSubmit(event) {
     event.preventDefault();
     const formData = new FormData(event.target);
-    console.log(formData.get('username'))
+    console.log(formData.get("username"));
 
-      const identifier =  formData.get("username")
-      const password =  formData.get("password")
+    const identifier = formData.get("username");
+    const password = formData.get("password");
 
     if (identifier === "" || password === "") {
       window.alert('Please enter valid input')
-    }else{
-      console.log('inside success')
-      mutationFun({variables:{
-        identifier,
-        password
-      }})
-        
 
-
-      // try {
-      //   const response = await fetch("http://localhost:1337/api/auth/local",{
-      //     method:"POST",
-      //     headers:{
-      //       "Content-Type": "application/json",
-      //       "Authorization" : "bearer" 
-  
-      //     },
-      //     body:JSON.stringify(userData)
-      //   })
-      //   const resData = await response.json()
-      //   console.log(resData)
-        
-
-      //   if(!response.ok){
-      //     throw new Error("Something went wrong...!")
-      //   }else if(response.ok){
-      //     console.log("Login successfully")
-      //     storeData(resData)
-      //     setTimeout(() => {
-      //       navigate("/")
-      //     }, 1000);
-      //   }
-      // } catch (error) {
-      //   window.alert(error) 
-      // }
+    } else {
+      mutationFun({
+        variables: {
+          identifier,
+          password,
+        },
+      });
     }
   }
   return (
     <>
       <div className="login-container">
         <div>
-        <form className="login-form" onSubmit={handleSubmit}>
-          <h2>Login</h2>
-          <div className="login-form-group">
-            <label className="login-lable" htmlFor="username">Username</label>
-            <input
-            className="login-input "
-              type="text"
-              id="username"
-              name="username"
-              placeholder="name or email"
-            />
-          </div>
-          <div className="login-form-group">
-            <label className="login-lable" htmlFor="password">Password</label>
-            <input
-            className="login-input "
-              type="password"
-              id="password"
-              name="password"
-              placeholder="password"
-            />
-          </div>
-          <button type="submit" className="login-btn">Login</button>
-          
-        </form>
-        <p className="registration-message">
+          <form className="login-form" onSubmit={handleSubmit}>
+            <h2>Login</h2>
+            <div className="login-form-group">
+              <label className="login-lable" htmlFor="username">
+                Username
+              </label>
+              <input
+                className="login-input "
+                type="text"
+                id="username"
+                name="username"
+                placeholder="name or email"
+              />
+            </div>
+            <div className="login-form-group">
+              <label className="login-lable" htmlFor="password">
+                Password
+              </label>
+              <input
+                className="login-input "
+                type="password"
+                id="password"
+                name="password"
+                placeholder="password"
+              />
+            </div>
+            <button type="submit" className="login-btn">
+              Login
+            </button>
+          </form>
+          <p className="registration-message">
             New user ?
             <span>
               <Link to="/register">Register</Link>
