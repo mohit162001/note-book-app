acdf7a1545448155b7bdbedfc356f156e7e18e96
Mohit Devade <mohit.deavade123@gmail.com>
2024-02-23 10:06:58 +0530

    button css updation

--- src/pages/NotePage.js
+++ src/pages/NotePage.js
@@ -1,9 +1,22 @@
 import React from 'react'
 import Note from '../components/Note'
+import { useParams } from 'react-router-dom'
+import { useQuery } from '@apollo/client'
+import { GET_NOTE } from '../query/query'
 
 function NotePage() {
+  const param = useParams()
+  const id = param.id
+  console.log(param.id)
+  const {data,loading,error} = useQuery(GET_NOTE,{
+    variables:{
+      id: id
+    }
+  })
+
+  console.log(data)
   return (
-    <Note/>
+    <Note data={id && data}/>
   )
 }
 
