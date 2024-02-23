3234fe41e4aa4e9055d1b210649e6d367cec64b4
Mohit Devade <mohit.deavade123@gmail.com>
2024-02-23 10:09:10 +0530

    edit note funtionality added

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,12 +1,34 @@
 import { useMutation } from "@apollo/client";
 import React from "react";
-import './CSS/note.css'
-import { CREATE_NOTE } from "../query/query";
+import "./CSS/note.css";
+import { CREATE_NOTE, UPDATE_NOTE } from "../query/query";
+import { useNavigate } from "react-router-dom";
 
-function Note() {
-  const [mutationFun,{}] = useMutation(CREATE_NOTE, {
+function Note({ data }) {
+// if(data){
+//   console.log(data.note.data.id)
+// }
+  const navigate = useNavigate();
+  const [mutationFun] = useMutation(CREATE_NOTE, {
     onCompleted(data) {
       console.log(data);
+      window.alert("Note created");
+      setTimeout(() => {
+        navigate("/history");
+      }, 1000);
+    },
+    onError(error) {
+      console.log(error);
+    },
+  });
+
+  const [updateFun] = useMutation(UPDATE_NOTE, {
+    onCompleted(data) {
+      console.log(data);
+      window.alert("Note updated");
+      setTimeout(() => {
+        navigate("/history");
+      }, 1000);
     },
     onError(error) {
       console.log(error);
@@ -22,55 +44,79 @@ function Note() {
     const date = currDate.toLocaleDateString();
     const id = localStorage.getItem("uid");
 
-    console.log(typeof id);
+    console.log(title);
+    console.log(content);
+    console.log(date)
 
-    console.log(title, content, date);
-
-    if (title === "") {
-      window.alert("note cannot has empty title");
-    } else {
-      mutationFun({
-        variables: {
-          title,
-          content,
-          date,
-          id,
-        },
-      });
+    if(data){
+      if (title === "") {
+        window.alert("note cannot has empty title");
+      } else {
+        console.log("id from inside ",data.note.data.id)
+        updateFun({
+          variables: {
+            noteId: data.note.data.id,
+            title:title,
+            content:content,
+            date:date,
+            id:id
+          }
+        });
+      }
+    }else{
+      if (title === "") {
+        window.alert("note cannot has empty title");
+      } else {
+        mutationFun({
+          variables: {
+            title,
+            content,
+            date,
+            id,
+          },
+        });
+      }
     }
   }
 
   return (
     <>
-      <div className="note-container" >
-      <h2 className="note-h2">Create your note here</h2>
+      <div className="note-container">
+        <h2 className="note-h2">Create your note here</h2>
         <div className="note-form-container">
-        <form className="note-form"  onSubmit={handleSubmit}>
-         
-          <div  className="note-title">
-            <label className="title-lable" >Title</label>
-            <input
-              className="title-input"
-              type="text"
-              id="title"
-              name="title"
-              placeholder="note title"
-            />
-          </div>
-          <div >
-            <label className="content-lable">Content</label>
-            <textarea
-              rows={20}
-              cols={80}
-              id="content"
-              name="content"
-              placeholder="content...."
-            />
-          </div>
-          <button className="note-submit" type="submit">
-            Sumbit
-          </button>
-        </form>
+          <form className="note-form" onSubmit={handleSubmit}>
+            <div className="note-title">
+              <label className="title-lable">Title</label>
+              <input
+                className="title-input"
+                type="text"
+                id="title"
+                name="title"
+                placeholder="note title"
+                defaultValue={data ? data.note.data.attributes.title : ""}
+              />
+            </div>
+            <div>
+              <label className="content-lable">Content</label>
+              <textarea
+                rows={20}
+                cols={80}
+                id="content"
+                name="content"
+                placeholder="content...."
+                defaultValue={data ? data.note.data.attributes.content : ""}
+              />
+            </div>
+            {data ? (
+             <button className="note-submit" type="submit" >
+                Update
+              </button>
+            ) : (
+              <button className="note-submit" type="submit">
+              Submit
+            </button>
+            )}
+          </form>
         </div>
       </div>
     </>
