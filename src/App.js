1f057c23540e932cee1e7b7777a35271ae5f7df7
Mohit Devade <mohit.deavade123@gmail.com>
2024-02-23 10:05:06 +0530

    changes regarding routes

--- src/App.js
+++ src/App.js
@@ -6,11 +6,12 @@ import ResgistrationPage from './pages/RegistrationPage.js'
 import { checkAuth } from './helper.js'
 import HistoryPage from './pages/HistoryPage.js'
 import NotePage from './pages/NotePage.js'
-import DefaultPage from './pages/DefaultPage.js'
+// import DefaultPage from './pages/DefaultPage.js'
 const router = createBrowserRouter([
   {path:'/',element:<RootPage/>,loader:checkAuth,children:[
-    {index:true,element:<DefaultPage/>},
-    {path:'note',element:<NotePage/>},
+    // {index:true,element:<DefaultPage/>},
+    {path:'/',element:<NotePage/>},
+    {path:'/:id',element:<NotePage/>},
     {path:'history',element:<HistoryPage/>}
   ]},
   {path:'/login',element:<LoginPage/>},
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,19 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import './CSS/sidebar.css';
 
 
 function SideBar() {
+  const [text,setText] = useState(true)
   return (
     <aside className="sidebar-container">
       <nav className="sidebar">   
         <p className='sidebar-title'>Note Book</p>       
         <ul className="sidebar-ul">
           <li className="sidebar-list-item">
-            <NavLink className={({ isActive }) => (isActive ? 'active' : 'inactive')} to="/note">New Note</NavLink>
+            <NavLink onClick={()=>setText(true)} className={({ isActive }) => (isActive ? 'active' : 'inactive')} to="/">{text ? 'New Note': "Add New Note"}</NavLink>
           </li>
           <li className="sidebar-list-item">
-            <NavLink className={({ isActive }) => (isActive ? 'active' : 'inactive')} to="/history">History</NavLink>
+            <NavLink onClick={()=>setText(false)} className={({ isActive }) => (isActive ? 'active' : 'inactive')} to="/history">History</NavLink>
           </li>
         </ul>
       </nav>
