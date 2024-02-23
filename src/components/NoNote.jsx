e2c833d450d8995a39f83e21197403961fe3d848
Mohit Devade <mohit.deavade123@gmail.com>
2024-02-23 16:38:19 +0530

    no note component updated

--- src/components/NoNote.jsx
+++ src/components/NoNote.jsx
@@ -1,20 +1,22 @@
-import React from 'react'
-import './CSS/notehistory.css'
-import { Link } from 'react-router-dom'
-import noNotes_img from '../assets/nonotes.png'
+import React from "react";
+import "./CSS/notehistory.css";
+import { Link } from "react-router-dom";
+import noNotes_img from "../assets/nonotes.png";
 function NoNote() {
   return (
     <div className="nonotes-container">
-            <img src={noNotes_img} alt="" />
-            <h2 id="nonotes-h2">No Data found</h2>
-            <p id="nonotes-p1">
-              Create a <span>Note</span> first
-            </p>
-            <p id="default-p2">
-              <button className="create-btn"><Link to='/'>Create note</Link></button>
-            </p>
-          </div>
-  )
+      <img src={noNotes_img} alt="" />
+      <h2 id="nonotes-h2">No Data found</h2>
+      <p id="nonotes-p1">
+        Create a <span>Note</span> first
+      </p>
+      <p id="default-p2">
+        <button className="create-btn">
+          <Link to="/">Create note</Link>
+        </button>
+      </p>
+    </div>
+  );
 }
 
-export default NoNote
+export default NoNote;
